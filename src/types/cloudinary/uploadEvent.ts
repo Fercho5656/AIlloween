0f92f136e677d6e0e124fbfeb18f6b216af2f3f7
Fernando Moreno <morenomoranfernando@gmail.com>
2024-10-11 01:18:56 -0600

    add type for photo upload

---
+++ src/types/cloudinary/uploadEvent.ts
@@ -0,0 +1,34 @@
+import type { CloudinaryUploadWidget } from '@cloudinary-util/types'
+
+export type CloudinaryUploadEvent = {
+  event: String
+  info: Info
+  UploadWidget: CloudinaryUploadWidget
+}
+
+interface Info {
+  id: string
+  batchId: string
+  asset_id: string
+  public_id: string
+  version: number
+  version_id: string
+  signature: string
+  width: number
+  height: number
+  format: string
+  resource_type: string
+  created_at: string
+  tags: string[]
+  bytes: number
+  type: string
+  etag: string
+  placeholder: boolean
+  url: string
+  secure_url: string
+  folder: string
+  access_mode: string
+  original_filename: string
+  path: string
+  thumbnail_url: string
+}
